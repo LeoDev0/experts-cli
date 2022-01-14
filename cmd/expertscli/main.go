09adb946a4f83dbf37b3291cac629ce794350b4b
Leonardo T. Fernandes <leo.80@protonmail.com>
2022-01-14 20:31:04 -0300

    feat: add main file to call commands and throw errors

--- cmd/expertscli/main.go
+++ cmd/expertscli/main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/leodev0/expertscli/commands"
+	"github.com/leodev0/expertscli/internal"
+)
+
+var commandList = []internal.Command{
+	new(commands.Start),
+}
+
 func main() {
-	
+	err := internal.CommandInit("expertscli").Start(commandList)
+
+	if err != nil {
+		fmt.Println(err.Error())
+	}
 }
