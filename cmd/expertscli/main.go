9d314dce0bb7b7a0cae6105f0d0e890e8ac02a60
Leonardo T. Fernandes <leo.80@protonmail.com>
2022-01-14 21:07:58 -0300

    refactor: add message import and fix start import to main file

--- cmd/expertscli/main.go
+++ cmd/expertscli/main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"fmt"
 
-	"github.com/leodev0/expertscli/commands"
+	"github.com/leodev0/expertscli/commands/message"
+	"github.com/leodev0/expertscli/commands/start"
 	"github.com/leodev0/expertscli/internal"
 )
 
 var commandList = []internal.Command{
-	new(commands.Start),
+	new(start.Start),
+	new(message.Message),
 }
 
 func main() {
